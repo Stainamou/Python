56f89d943a942d463cb3a4687fc2b09f71a368f2
Stainamou <margussten@gmail.com>
2024-08-20 14:51:39 +0300

    Delete mod-02/teht4.py

--- mod-02/teht4.py
+++
@@ -1,6 +0,0 @@
-import math
-
-kanta = float(input("Anna kanta:  "))
-korkeus = float(input("Anna korkeus:  "))
-pinta_ala = (float(kanta * korkeus))
-print ("Suorakulman pinta ala on: " + str(pinta_ala) + "m^2")
