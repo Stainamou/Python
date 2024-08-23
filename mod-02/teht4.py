e99cfb930da794b0b1963ff59424cc1cd4ce6d8c
Stainamou <margussten@gmail.com>
2024-08-23 09:38:49 +0300

    Update teht4.py

--- mod-02/teht4.py
+++ mod-02/teht4.py
@@ -1,10 +1,11 @@
 import math
 
 luku1 = int(input("Anna ensimmäinen luku: "))
-luku2 = int(input("Anna toinen luku:"))
-summa = luku1 + luku2
-tulo = luku1 * luku2
-keskiarvo = summa / 2
+luku2 = int(input("Anna toinen luku: "))
+luku3 = int(input("Anna kolmas luku: "))
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keskiarvo = summa / 3
 print("Lukujen summa on: ", summa)
 print("Lukujen tulo on: ", tulo)
 print("Lukujen keskiarvo on: ", keskiarvo)
