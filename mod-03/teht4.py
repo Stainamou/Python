4e98ea98cba194d1efbe6774310f62a73a542613
Stainamou <margussten@gmail.com>
2024-08-28 14:34:51 +0300

    Update teht4.py

--- mod-03/teht4.py
+++ mod-03/teht4.py
@@ -1,10 +1,10 @@
 vuosi = int(input("Kerro vuosi: "))
 
-if vuosi % 4 == 0:
+if vuosi % 400 == 0:
     print(vuosi, "on karkausvuosi")
 elif vuosi % 100 == 0:
     print(vuosi, "ei ole karkausvuosi")
-elif vuosi % 400 == 0:
+elif vuosi % 4 == 0:
     print(vuosi, "on karkausvuosi")
 else:
     print(vuosi, "ei ole karkausvuosi")
