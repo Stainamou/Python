bcd017e6b1f13a11df956a5a2b799dfd343b3059
Stainamou <margussten@gmail.com>
2024-09-03 13:27:49 +0300

    Update teht1.py

--- mod-05/teht1.py
+++ mod-05/teht1.py
@@ -1,6 +1,3 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän. Ohjelma heittää
-# kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan. Käytä
-# for-toistorakennetta.
 import random
 
 lukumäärä = int(input("Anna arpakuutioiden lukumäärä: "))
