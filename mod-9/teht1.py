fe2ef1dffed5b03f15cd31eac9e412b77b986a21
Stainamou <margussten@gmail.com>
2024-10-24 16:48:46 +0300

    Delete mod-9 directory

--- mod-9/teht1.py
+++
@@ -1,9 +0,0 @@
-class Auto:
-    def __init__(self, rekisteritunnus, huippunopeus, hetk_nopeus, kulj_matka):
-        self.rekisteritunnus = rekisteritunnus
-        self.huippunopeus = huippunopeus
-        self.hetk_nopeus = hetk_nopeus
-        self.kulj_matka = kulj_matka
-
-auto = Auto("ABC-123", 142, 0, 0)
-print(f"Rekisteritunnus: {auto.rekisteritunnus:s}, huippunopeus: {auto.huippunopeus:d} km/h, hetkellinen nopeus: {auto.hetk_nopeus:d} km/h, kuljettu matka: {auto.kulj_matka:d} km")
